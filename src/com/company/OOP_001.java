d6079236c4baffa29500a6a8aea1db20feb6fe9f
Roman Vodnev <forfun90@mail.ru>
2019-02-26 00:48:22 +0300

    Добавлен java-файл ООП_001

---
+++ src/com/company/OOP_001.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class OOP_001 {
+    public static void main(String[] args) {
+    // write your code here
+    System.out.println(squearSum(10,20)+1);
+
+}
+    static long squearSum(int z, int y) {
+        return z*z + y*y;
+    }
+}
