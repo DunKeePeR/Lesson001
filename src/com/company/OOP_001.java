8e095b370f17ae5d4223e4b9758045a6f7245fd9
Roman Vodnev <forfun90@mail.ru>
2019-02-27 13:17:40 +0300

    Добавлен файл "Вопрос по Java". Изменен джава-файл ООП_001.

--- src/com/company/OOP_001.java
+++ src/com/company/OOP_001.java
@@ -2,10 +2,30 @@ package com.company;
 
 public class OOP_001 {
     public static void main(String[] args) {
-    System.out.println(squearSum(10,20)+1);
-
+        Dog dog1 = new Dog("Тузик", 22); // Собака по кличке Тузик, возраст 2 года
+        dog1.getAge();
+        dog1.vozrast();
+        dog1.setAge(20);
+        dog1.vozrast();
+    }
 }
-    static long squearSum(int z, int y) {
-        return z*z + y*y;
+class	Dog {
+    private int age;// возраст
+    private String name; // кличка
+
+    public Dog(String n, int a) {
+        name = n;
+        age = setAge(a);
+    }
+    public void vozrast () {
+        System.out.println(age);
+    }
+    public void setAge (int newAge) {
+        if (newAge < 0) System.out.println("Как это понимать? Собака еще не родилась?");
+        else if (newAge > 20) System.out.println("Они столько не живут");
+        else age = newAge;
+    }
+    public int getAge() {
+        return age;
     }
 }
